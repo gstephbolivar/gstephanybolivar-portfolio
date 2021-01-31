281409c5dccc5111f0407292b3a657a468663fe6
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-01-30 19:13:53 -0500

    Basic set up for all pages

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import NavBar from "../../components/NavBar/NavBar";
+
+const Home = () => {
+    return (
+        <>
+           {/* <NavBar />  */}
+           <h1>This is my home page</h1>
+        </>
+    );
+};
+
+export default Home;

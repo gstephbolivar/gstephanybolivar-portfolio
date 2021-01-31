b3ae3ccffac66b90c563167c04a6a6d0f90112fe
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-01-31 16:25:09 -0500

    Portfolio page finished

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -22,42 +22,38 @@ const useStyles = makeStyles({
 const Project = (props) => {
   const classes = useStyles();
   return (
-      <Box component="div">
-        <Grid container justify="center">
-          <Grid item xs={12} sm={8} md={6}>
-            <Card className={classes.cardContainer}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt={props.title}
-                  height="auto"
-                  image={process.env.PUBLIC_URL + props.image}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5">
-                    {props.title}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {props.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  GitHub Repository
-                </Button>
-                <Button size="small" color="primary">
-                  Deployed Site
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+    <Box component="div">
+      <Grid container justify="center">
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt={props.title}
+                height="auto"
+                image={process.env.PUBLIC_URL + props.image}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  {props.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {props.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary" href={props.repo}>
+                GitHub
+              </Button>
+              <Button size="small" color="primary" href={props.deploy}>
+                Live
+              </Button>
+            </CardActions>
+          </Card>
         </Grid>
-      </Box>
+      </Grid>
+    </Box>
   );
 };
 

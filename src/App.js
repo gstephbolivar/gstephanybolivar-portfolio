5b47f9e657de22caf5cbbc4a6a1dc9f4b31face9
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-01-30 19:15:30 -0500

    Create routes for pages on app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { HashRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Contact from "./pages/Contact/Contact";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-import "./App.css";
 
 function App() {
   return (
     <Router basename="/">
       <CssBaseline />
-      <Home />
+      <NavBar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/portfolio" component={Portfolio}/>
+        <Route path="/contact" component={Contact}/>
+      </Switch>
     </Router>
   );
 }

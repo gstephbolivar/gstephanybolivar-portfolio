bf33a4fc851c5aa6131062910654412d9aed435f
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-01 13:41:55 -0500

    Contact page set up

---
+++ src/components/ContactBox/ContactBox.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Grid, Card, CardContent, Typography } from "@material-ui/core";
+import EmailIcon from "@material-ui/icons/Email";
+import SmartphoneIcon from "@material-ui/icons/Smartphone";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import FormatAlignJustifyIcon from '@material-ui/icons/FormatAlignJustify';
+
+const useStyles = makeStyles({
+  cardContainer: {
+    opacity: "0.5",
+    color: "red",
+  },
+});
+const ContactBox = () => {
+  const classes = useStyles();
+  return (
+    <Box component="div">
+      <Grid container justify="center">
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardContent>
+              <Typography variant="h4"><EmailIcon/>Email:</Typography>
+              <Typography variant="h5">  <a href="mailto: g.stephanybolivar@gmail.com"> g.stephanybolivar@gmail.com</a></Typography>
+              <br />
+              <Typography variant="h4"><SmartphoneIcon/>Phone Number:</Typography>
+              <Typography variant="h5"><a href="tel:770-789-3209">770-789-3209</a></Typography>
+              <br />
+              <Typography variant="h4"><GitHubIcon/>GitHub:</Typography>
+              <Typography variant="h5"><a href="https://github.com/gstephbolivar"> @gstephbolivar </a></Typography>
+              <br />
+              <Typography variant="h4"><LinkedInIcon/>LinkedIn:</Typography>
+              <Typography variant="h5"><a href="https://www.linkedin.com/in/gstephanybolivar/">G. Stephany Bolivar</a></Typography>
+              <br />
+              <Typography variant="h4"><FormatAlignJustifyIcon/>Resume:</Typography>
+              <Typography variant="h5"><a href={process.env.Public_URL +"/assets/Resume/G.StephanyBolivar.resume.pdf"} download="G. Stephany Bolivar Resume">View/Download</a></Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default ContactBox;
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Box, Grid, Card, Typography, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ContactBox from "../../components/ContactBox/ContactBox";
 
+const useStyles = makeStyles({
+    titleContainer: {
+        opacity: "0.5",
+        marginTop: "3rem",
+        marginBottom: "3rem"
+    },
+    titleFont: {
+        fontWeight: "bold",
+        textAlign: "center"
+    }
+})
 const Contact = () => {
-    return (
-        <div>
-            <h1>This is my contact page</h1>
-        </div>
-    );
+    const classes= useStyles();
+  return (
+      <>
+    <Box component="div">
+      <Grid container justify="center">
+        <Grid item xs={12} sm={8} md={6} >
+          <Card className={classes.titleContainer}>
+            <CardContent>
+              <Typography className={classes.titleFont} variant="h2">Let's Create Something</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+    <Box component="div">
+        <ContactBox />
+    </Box>
+    </>
+  );
 };
 
 export default Contact;
